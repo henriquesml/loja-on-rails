47180ea000a77e215294de8584da231a3e19f242
henriquesml <henrique_schmeller@hotmail.com>
2019-12-13 15:50:28 -0300

    criando busca na view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root to: "produtos#index"
   resources :produto, only: [:new, :create, :destroy]
+  get "produtos/busca", to: "produtos#busca", as: :busca_produto
 end
