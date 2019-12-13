f9b502cb6a9d255a16bdbf48ccb5b28524eb6a55
henriquesml <henrique_schmeller@hotmail.com>
2019-12-13 16:10:41 -0300

    criação de busca de itens da loja

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -11,8 +11,12 @@ class ProdutosController < ApplicationController
     end
     def destroy
         id = params[:id]
-
         Produto.destroy id
         redirect_to root_url
+    end
+
+    def busca
+        @nome = params[:nome]
+        @produtos = Produto.where "nome like ?", "%#{@nome}%"
     end
 end
